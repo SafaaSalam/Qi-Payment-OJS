f120069402ad8680a5d697f827db59cf26549066
Safaa Salam <safaaalatabbi@hotmail.com>
2026-06-01 12:44:50 +0300

    First Commit
    First Commit

---
+++ QiPaymentPlugin.php
@@ -0,0 +1,42 @@
+msgid ""
+msgstr ""
+"Project-Id-Version: \n"
+"Report-Msgid-Bugs-To: \n"
+"POT-Creation-Date: 2026-05-31T07:19:00+00:00\n"
+"PO-Revision-Date: 2026-05-31T07:19:00+00:00\n"
+"Last-Translator: \n"
+"Language-Team: \n"
+"Language: en\n"
+"MIME-Version: 1.0\n"
+"Content-Type: text/plain; charset=UTF-8\n"
+"Content-Transfer-Encoding: 8bit\n"
+
+msgid "plugins.paymethod.qi.displayName"
+msgstr "Qi Payment Gateway"
+
+msgid "plugins.paymethod.qi.description"
+msgstr "Payments will be processed using the Qi Payment Gateway."
+
+msgid "plugins.paymethod.qi"
+msgstr "Qi Payment Gateway"
+
+msgid "plugins.paymethod.qi.settings"
+msgstr "Qi Payment Gateway Settings"
+
+msgid "plugins.paymethod.qi.settings.testMode"
+msgstr "Sandbox Test Mode"
+
+msgid "plugins.paymethod.qi.settings.apiHost"
+msgstr "Custom API Host"
+
+msgid "plugins.paymethod.qi.settings.terminalId"
+msgstr "Terminal ID"
+
+msgid "plugins.paymethod.qi.settings.username"
+msgstr "Username"
+
+msgid "plugins.paymethod.qi.settings.password"
+msgstr "Password"
+
+msgid "plugins.paymethod.qi.error"
+msgstr "A transaction error occurred. Please contact the journal manager for details."
